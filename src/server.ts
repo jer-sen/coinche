3692a0d070cfff2669ecea7f2a91fe44f2bbd968
Jérôme <jerome.st@gmail.com>
2020-03-30 11:30:22 +0200

    full stack

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,112 @@
 import express from 'express';
+import { ObjectID, MongoClient } from 'mongodb';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-const PORT = process.env.PORT || 3000;
-const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+/*
+créer partie
+rejoindre partie
+
+changer nom
+
+mélanger
+
+couper
+distribuer
+
+mettre devant
+regarder 4 dernières
+jouer carte
+reprendre carte
+prendre le pli
+annuler prise
+regarder tout
+rassembler
+
+dernières actions
+
+
+affichage :
+- names
+- hand
+- current
+- lastWinned
+- winned avec points (sans atouts)
+- lastActions
+
+
+{
+	lastActions: ["Jérôme à pris le pli"],
+	hands: [
+		[{ color: 'heart', value: 'ace' }],
+		[{ color: 'tile', value: '7' }],
+		[{ color: 'clover', value: 'king' }],
+		[{ color: 'pike', value: '8' }],
+	],
+	winned: [
+		[{ color: 'heart', value: 'queen' }],
+		[{ color: 'heart', value: 'jack' }],
+	],
+	current: [{ player: 0, color: 'heart', value: '8' }],
+	lastWinned: [{ color: 'heart', value: '8' }],
+	toDeal: [{ }]
+}
+*/
+
+
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+
+(async () => {
+	const MONGODB_URI = process.env['MONGODB_URI'] || 'mongodb://heroku_36d4mbhj:qskglj6r4o81stnudp32vj2rp2@ds343718.mlab.com:43718/heroku_36d4mbhj';
+	const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+	const dbName = MONGODB_URI.match(/^.*\/([a-zA-Z0-9_]+$)/u)[1];
+	const db = client.db(dbName);
+	
+	const server = new ApolloServer({ typeDefs, resolvers });
+	const graphqlPath = '/graphql';
+
+
+	const PORT = process.env.PORT || 3000;
+	const app = express();
+
+	app.use('/', express.static('frontdist'));
+	server.applyMiddleware({
+		app: app,
+		path: graphqlPath,
+	});
+	app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+})();
 
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+module.exports = (env, argv) => {
+	const isDev = argv.$0.endsWith('webpack-dev-server.js');
+	console.log("MODE DEV : " + isDev);	
+	return {
+		devServer: {
+			contentBase: path.join(__dirname, 'frontdist'),
+			compress: true,
+			port: 9000
+		},
+		
+		mode: isDev ? 'development' : 'production',
+
+		// Enable sourcemaps for debugging webpack's output.
+		devtool: "source-map",
+
+		entry: './frontsrc/index.tsx',
+		output: {
+			path: path.resolve(__dirname, 'frontdist'),
+			filename: 'main.js'
+		},
+		
+		resolve: {
+				// Add '.ts' and '.tsx' as resolvable extensions.
+				extensions: [".ts", ".tsx", '.js', '.jsx']
+		},
+
+		plugins: [
+			new HtmlWebpackPlugin({
+				title: "Ptite coinche",
+				template: './index.html',
+			}),
+			new CopyPlugin([
+				// react.production.min
+				{ from: './node_modules/react/umd/react.' + (isDev ? 'development' : 'production') + '.js', to: 'react.js' },
+				{ from: './node_modules/react-dom/umd/react-dom.' + (isDev ? 'development' : 'production') + '.js', to: 'react-dom.js' },
+			]),
+		],
+
+		module: {
+				rules: [
+						{
+								test: /\.ts(x?)$/,
+								exclude: /node_modules/,
+								use: [
+										{
+												loader: "ts-loader",
+												options: {
+													configFile: '../tsconfigFront.json',
+												},
+										}
+								]
+						},
+						// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+						{
+								enforce: "pre",
+								test: /\.js$/,
+								loader: "source-map-loader"
+						}
+				]
+		},
+
+		// When importing a module whose path matches one of the following, just
+		// assume a corresponding global variable exists and use that instead.
+		// This is important because it allows us to avoid bundling all of our
+		// dependencies, which allows browsers to cache those libraries between builds.
+		externals: {
+			"react": "React",
+			"react-dom": "ReactDOM"
+		},
+	};
+};
