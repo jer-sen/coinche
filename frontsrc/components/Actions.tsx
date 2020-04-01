85fa06ebd471560c3e4f681816bde4cf288ffe0c
Jérôme <jerome.st@gmail.com>
2020-04-01 17:58:00 +0200

    list players

--- frontsrc/components/Actions.tsx
+++ frontsrc/components/Actions.tsx
@@ -5,6 +5,12 @@ import { observer } from "mobx-react";
 
 export default observer(() =>
 	<div>
+		<ol start={0} >
+			{
+				// eslint-disable-next-line react/no-array-index-key
+				globalStore.players.map((p: string, i: number) => <li key={i}>{p}</li>)
+			}
+		</ol>
 		<ul>
 			{
 				[...globalStore.actions].reverse().slice(0, 20).map((a: string) => <li key={a}>{a}</li>)
